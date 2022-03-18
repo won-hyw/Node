38788062810a11331af4978e5f323dfad47771fe
won-hyw <w2019@e-mirim.hs.kr>
2022-03-18 13:59:00 +0900

    함수 연습

---
+++ MoonBub/function.js
@@ -0,0 +1,14 @@
+function 문자출력함수(문자) {
+    console.log(문자);
+}
+
+문자출력함수('PC지키미');
+
+// printChar = 문자출력함수;
+// printChar('b');
+
+printChar = function(문자) {
+    console.log(문자);
+}
+
+printChar('청소를 열심히 합시다');
