601de5c809d76fd1398f8aa451ef79e7acbb249f
won-hyw <w2019@e-mirim.hs.kr>
2022-03-25 15:06:31 +0900

    web2 동적 서버 페이지

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 
 // 모든 node 웹 서버 애플리케이션은 웹 서버 객체를 만들어야 함
 // call back 함수
 const app = http.createServer(function (req, res) {
-    let url = req.url;
-
-    if(url === '/')
+    let _url = req.url;
+    let queryData = url.parse(_url, true).query;
+    res.end(queryData.id);
+  /*  if(url === '/')
         url = '/index.html';
     if(url === '/favicon.ico')
         return res.writeHead(404);
     res.writeHead(200);
-    res.end(fs.readFileSync(__dirname + url));
+    res.end(fs.readFileSync(__dirname + url)) */
 });
 
 app.listen(3333);
