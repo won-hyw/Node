d29374e5ddd2c79b280d10ae0dc527b7fbb3c8bc
won-hyw <w2019@e-mirim.hs.kr>
2022-03-25 14:46:26 +0900

    web2-5 정적 서버 페이지

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const http = require('http')
+const http = require('http');
+const fs = require('fs');
 
-
+// 모든 node 웹 서버 애플리케이션은 웹 서버 객체를 만들어야 함
+// call back 함수
 const app = http.createServer(function (req, res) {
+    let url = req.url;
+
+    if(url === '/')
+        url = '/index.html';
+    if(url === '/favicon.ico')
+        return res.writeHead(404);
     res.writeHead(200);
-    console.log(__dirname + req.url);
-    res.end("Hello World");
+    res.end(fs.readFileSync(__dirname + url));
 });
 
-app.listen(3000);
+app.listen(3333);
