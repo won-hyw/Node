2389cadb405ee928e2eb201e668ddff80c531829
won-hyw <w2019@e-mirim.hs.kr>
2022-03-28 15:10:10 +0900

    web2-13 readFile로 본문 구현

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const fs = require('fs');
 const url = require('url');
 
 // 모든 node 웹 서버 애플리케이션은 웹 서버 객체를 만들어야 함
-// call back 함수
+// call back 함수 : 클라이언트가 웹페이지를 서버한테 요청할 때
 const app = http.createServer(function (req, res) {
     let _url = req.url;
-    let queryData = url.parse(_url, true).query;
-    res.end(queryData.id);
+    let queryData = url.parse(_url, true).query; // Object Type을 JSON Type으로 변경해준다
+    res.end(queryData.id); // id를 웹페이지 화면에 보여준다
   /*  if(url === '/')
         url = '/index.html';
     if(url === '/favicon.ico')
---
+++ main.js
@@ -0,0 +1,65 @@
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
+
+const app = http.createServer(function (request, response) {
+    const _url = request.url
+    const queryData = url.parse(_url, true).query
+    const pathname = url.parse(_url, true).pathname
+    if (pathname === '/') {
+        if (queryData.id === undefined) {
+            const title = 'Welcome'
+            const description = 'Hello, Node.js'
+            const template = `
+<!doctype html>
+<html lang="ko">
+<head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h1><a href="/">WEB</a></h1>
+    <ul>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JavaScript">JavaScript</a></li>
+    </ul>
+    <h2>${title}</h2>
+    <p>${description}</p>
+</body>
+</html>
+`
+            response.writeHead(200)
+            response.end(template)
+        } else {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+                const title = queryData.id
+                const template = `
+<!doctype html>
+<html lang="ko">
+<head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h1><a href="/">WEB</a></h1>
+    <ul>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JavaScript">JavaScript</a></li>
+    </ul>
+    <h2>${title}</h2>
+    <p>${description}</p>
+</body>
+</html>
+`
+                response.writeHead(200)
+                response.end(template)
+            })
+        }
+    } else {
+        response.writeHead(404)
+        response.end('Not found')
+    }
+})
+app.listen(3333)
