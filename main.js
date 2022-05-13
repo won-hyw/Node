f87285dfe6984cfd31bfdd38161adb2d760e3fa7
w2019 <w2019@e-mirim.hs.kr>
2022-05-13 15:02:55 +0900

    template 라이브러리 파일 분할

--- main.js
+++ main.js
@@ -3,35 +3,7 @@ const fs = require('fs');
 const url = require('url');
 const qs = require('querystring');
 const sanitizeHtml = require('sanitize-html');
-
-const template = {
-    List : function (fileList) {
-        let list = '<ul>';
-        for (let i = 0; i < fileList.length; i++) {
-            list += `<li><a href="/?id=${fileList[i]}">${fileList[i]}</a></li>`;
-        }
-        list += '</ul>';
-        return list
-    },
-    HTML : function (title, list, body, control) {
-        return `
-            <!doctype html>
-            <html lang="ko">
-            <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1><a href="/">WEB</a></h1>
-                ${list}
-                ${control}
-                <h2>${title}</h2>
-                <p>${body}</p>
-            </body>
-            </html>
-           `
-    }
-}
+const template = require('./lib/template.js');
 
 const app = http.createServer(function (request, response) {
     const _url = request.url
