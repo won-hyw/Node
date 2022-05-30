f4d44bb06e36ca6c55c5c9d5176fc175dbaddc56
w2019 <w2019@e-mirim.hs.kr>
2022-05-30 14:50:24 +0900

    web3-9.1 미들웨어의 사용(body-parser)

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const template = require('./lib/template.js');
 const fs = require('fs');
-const qs = require("querystring");
+// const bodyParser = require('body-parser');
+
+app.use(express.urlencoded({extended:false}));
 
 app.get('/', function (req, res) {
     fs.readdir('./data', function(err, filelist){
@@ -55,17 +57,11 @@ app.get('/create', function (req, res){
 });
 
 app.post('/create_process', function(req, res){
-    let body = '';
-    req.on('data', function (data) {
-        body = body + data;
-    });
-    req.on('end', function () {
-        const post = qs.parse(body);
-        const title = post.title;
-        const description = post.description;
-        fs.writeFile(`data/${title}`, description, 'utf-8', function (err) {
-            res.redirect(`/page/${title}`);
-        });
+    const reqBody = req.body;
+    const title = reqBody.title;
+    const description = reqBody.description;
+    fs.writeFile(`data/${title}`, description, 'utf-8', function (err) {
+        res.redirect(`/page/${title}`);
     });
 });
 
@@ -96,34 +92,22 @@ app.get('/update/:pageId', function(req, res){
 });
 
 app.post('/update_process', function (req, res) {
-    let body = '';
-    req.on('data', function (data) {
-        body = body + data;
-    });
-    req.on('end', function () {
-        const post = qs.parse(body);
-        const id = post.id;
-        const title = post.title;
-        const description = post.description;
-        fs.rename(`data/${id}`, `data/${title}`, function (error) {
-            fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-                res.redirect(`/page/${title}`);
-            });
+    const reqBody = req.body;
+    const id = reqBody.id;
+    const title = reqBody.title;
+    const description = reqBody.description;
+    fs.rename(`data/${id}`, `data/${title}`, function (error) {
+        fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+            res.redirect(`/page/${title}`);
         });
     });
 });
 
 app.post('/delete_process', function(req, res){
-    let body = '';
-    req.on('data', function (data) {
-        body = body + data;
-    });
-    req.on('end', function () {
-        const post = qs.parse(body);
-        const id = post.id;
-        fs.unlink(`data/${id}`, function (error) {
-            res.redirect(`/`);
-        });
+    const reqBody = req.body;
+    const id = reqBody.id;
+    fs.unlink(`data/${id}`, function (error) {
+        res.redirect(`/`);
     });
 });
 
