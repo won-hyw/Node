3779121619e218c8a2fad63ae23e0087b845e358
w2019 <w2019@e-mirim.hs.kr>
2022-06-13 14:23:32 +0900

    DB 연결

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+require('dotenv/config')
 
 const app = express()
+mongoose.connect(process.env.DB_CONNECTION
+    , {useNewUrlParser : true, useUnifiedTopology : true})
+const db = mongoose.connection;
+
+db.on('error', function (err) {
+    console.log(err)
+})
+
+db.once('open', function () {
+    console.log('DB 연결')
+})
 
 // morgan : HTTP request 기록해주는 미들웨어
 app.use(morgan('dev'))
